1e153fff3bfd7a554f9375abbe3e866a55e4b501
moongyuxx <gyuwon822@gmail.com>
2024-12-17 17:19:15 +0900

    import AddressBook to main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-def menu():
+from AddressBook import AddressBook
+
+def print_menu():
     print("1. 연락처 입력")
     print("2. 모든 연락처 출력")
     print("3. 연락처 검색")
     print("4. 연락처 삭제")
     print("5. 종료")
-    menu = input("메뉴선택: ")
+    menu = int(input("메뉴선택: "))
     return menu
     
 def main():
+    address = AddressBook()
     while True:
         menu = print_menu()
         if menu == 1:
+            address.add_address()
         elif menu == 2:
+            address.print_address()
         elif menu == 3:
+            address.search_address()
         elif menu == 4:
+            address.delete_address()
         elif menu == 5:
             break
         else:
