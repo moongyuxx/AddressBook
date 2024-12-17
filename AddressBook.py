1632a683c23f3e313fb6923ceff4032bdf4d3fae
moongyuxx <gyuwon822@gmail.com>
2024-12-17 16:52:40 +0900

    Add print_address to AddressBook
    *show all addresses

--- AddressBook.py
+++ AddressBook.py
@@ -9,4 +9,10 @@ class AddressBook:
         email = input("이메일: ")
         self.addresses[name] = {"phone": phone, "email": email}
 
- 
+    def print_address(self):
+        if not self.addresses:
+            print("저장된 연락처 없음")
+        else:
+            for name, i in self.addresses.items():
+                print(f"{name}  {i['phone']}  {i["email"]}\n")
+                
