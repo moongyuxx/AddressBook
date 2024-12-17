f31d195d46e605c96abfadedd4bc781034ccdad1
moongyuxx <gyuwon822@gmail.com>
2024-12-17 16:45:52 +0900

    Add add_address function to AddressBook

--- AddressBook.py
+++ AddressBook.py
@@ -1,6 +1,12 @@
 class AddressBook:
     def __init__(self):
-        self.addresses = {}
-        
+        self.addresses = {}    
 
-                
+    def add_address(self):
+    
+        name = input("이름: ")
+        phone = input("전화번호: ")
+        email = input("이메일: ")
+        self.addresses[name] = {"phone": phone, "email": email}
+
+ 
