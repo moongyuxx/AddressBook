68fc330e887596b9c99526a5fe55b6750f199989
moongyuxx <gyuwon822@gmail.com>
2024-12-17 16:56:53 +0900

    Add search_address function to AddressBook
    * search address by name

--- AddressBook.py
+++ AddressBook.py
@@ -11,8 +11,16 @@ class AddressBook:
 
     def print_address(self):
         if not self.addresses:
-            print("저장된 연락처 없음")
+            print("저장된 연락처 없음\n")
         else:
             for name, i in self.addresses.items():
-                print(f"{name}  {i['phone']}  {i["email"]}\n")
-                
+                print(f"{name}  {i["phone"]}  {i["email"]}\n")
+
+    def search_address(self):
+        name_search = input("검색할 이름: ")
+        if name_search in self.addresses:
+            address = self.addresses["name_search"]
+            print(f"{name_search}  {address["phone"]}  {address["email"]}\n")            
+        else:
+            print("저장된 연락처 없음\n")
+        
